825458059a557242f946fe5d3f58b7693ac58a97
NickD-BHSc <duannd@myumanitoba.ca>
2020-05-01 21:02:18 -0500

    MERN full stack web app

---
+++ backend/routes/users.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+let User = require('../models/user.model');
+
+router.route('/').get((req, res) => {
+    User.find()
+        .then(users => res.json(users))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => { //req.body take in info from the form and saves it as the json object for use in username
+    const username = req.body.username;
+
+    const newUser = new User({username});
+
+    newUser.save()
+        .then(() => res.json('User added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
